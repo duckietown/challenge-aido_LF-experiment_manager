b068d7f23534632e7a198fb2cc39ac2acea9cb33
Andrea Censi <acensi@ethz.ch>
2020-10-30 11:38:32 +0100

    Bump version: 6.0.8 → 6.0.9

--- experiment_manager.py
+++ experiment_manager.py
@@ -56,7 +56,7 @@ from duckietown_world.rules.rule import EvaluatedMetric
 from webserver import WebServer
 
 logger = ZLogger("experiment_manager")
-__version__ = "6.0.8"
+__version__ = "6.0.9"
 
 logger.info(f"experiment_manager {__version__}")
 
