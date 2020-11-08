ce26183f4bbe7c651d92771a16614c43051439a0
Andrea Censi <acensi@ethz.ch>
2020-11-08 23:44:46 +0100

    fix

--- makegif.sh
+++ makegif.sh
@@ -2,7 +2,8 @@
 input=$1
 tmp1=$1.tmp.mp4
 palette=$1.palette.png
-output=$1.output.gif
+output=$1.gif
 ffmpeg -i $input -vf scale=160x120 -r 10 -y $tmp1
 ffmpeg -i $tmp1 -vf palettegen=max_colors=256 -y $palette
 ffmpeg -i $input  -i $palette -lavfi paletteuse=dither=bayer -y $output
+rm -f $tmp1 $palette
