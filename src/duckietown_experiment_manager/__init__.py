77a3e1eacf8c73be42b77b58f7e53bf4530f1e3f
Andrea Censi <acensi@ethz.ch>
2020-11-14 00:56:26 +0100

    minor

--- src/duckietown_experiment_manager/__init__.py
+++ src/duckietown_experiment_manager/__init__.py
@@ -3,6 +3,6 @@ from zuper_commons.logs import ZLogger
 __version__ = "6.0.44"
 
 logger = ZLogger(__name__)
-logger.info(f"{__version__}")
+logger.debug(f"duckietown_experiment_manager version {__version__}")
 from .code import *
 from .experiment_manager import *
