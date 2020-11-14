b97e87710fd34fdfb819331b028884bd4086efc2
Andrea Censi <acensi@ethz.ch>
2020-11-14 17:28:20 +0100

    Bump version: 6.0.46 → 6.0.47

--- src/duckietown_experiment_manager/__init__.py
+++ src/duckietown_experiment_manager/__init__.py
@@ -1,6 +1,6 @@
 from zuper_commons.logs import ZLogger
 
-__version__ = "6.0.46"
+__version__ = "6.0.47"
 
 logger = ZLogger(__name__)
 logger.debug(f"duckietown_experiment_manager version {__version__}")
