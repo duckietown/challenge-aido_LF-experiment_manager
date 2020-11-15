33acf273c9b1e5ff81955f289d556f10d1fc6b04
Andrea Censi <acensi@ethz.ch>
2020-11-16 00:26:14 +0100

    fix

--- src/duckietown_experiment_manager/__init__.py
+++ src/duckietown_experiment_manager/__init__.py
@@ -3,6 +3,6 @@ from zuper_commons.logs import ZLogger
 __version__ = "6.0.49"
 
 logger = ZLogger(__name__)
-logger.debug(f"duckietown_experiment_manager version {__version__}")
+logger.debug(f"duckietown_experiment_manager version {__version__} path {__file__}")
 from .code import *
 from .experiment_manager import *
